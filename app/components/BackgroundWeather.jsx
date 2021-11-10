cff97e7995be9d9da276b5e6a5fc13a2e4496800
yamil-90 <yamil.coloma90@gmail.com>
2021-11-10 11:49:28 -0300

    modifique la funcion de backgroundWeather para que sea imagen de fondo

--- app/components/BackgroundWeather.jsx
+++ app/components/BackgroundWeather.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
-import { ImageBackground, View, Image, StyleSheet } from 'react-native';
 
 
 import sunny from "../../assets/weatherBackground/sunny.jpg";
 import rain from "../../assets/weatherBackground/rain.jpg";
 import cloudy from "../../assets/weatherBackground/cloudy.jpg";
 
-export default function BackgroundWeather({weather}){
+export default function BackgroundWeather(weather){
   
-    let source = ''
     switch (weather) {
         case 'Clouds':
-            source = cloudy
-            break;
+          return cloudy
+          break;
         case 'Clear':
-            source = sunny
-            break;
+          return sunny
+          break;
         case 'Rain':
-            source = rain
-            break;
+          return rain
+          break;
         default:
-            source = sunny
-            break;
+          return sunny
+          break;
+      }
+
     }
-    return (  
-        <Image source={source} style={Styles.background}>
-            
-        </Image>
-        
-
-    )}
-
-     const Styles = StyleSheet.create({
-        background:{
-         width:500,
-         height:800,   
-        
-        flex: 1,
-        position: 'absolute'
-        
-    }
-
-     })
-
 
     
