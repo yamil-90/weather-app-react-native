7ab3e93bdc8e05c3c09dee17583ec143720e3f7b
yamil-90 <yamil.coloma90@gmail.com>
2021-11-12 15:05:14 -0300

    agregue unas imagenes para la explicacion del funcionamiento de la app y un gradiente para home asi no se ve tan feo

--- app/screens/Home/Home.jsx
+++ app/screens/Home/Home.jsx
@@ -2,22 +2,38 @@ import React from "react";
 import { Text } from "react-native-elements";
 import { StyleSheet, View } from "react-native";
 import NearWeather from "../../components/NearWeather";
+import {SliderBox} from "react-native-image-slider-box"
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Home() {
+    const images= [
+        require("../../../assets/howToUseImages/misCiudades-text.png"),
+        require("../../../assets/howToUseImages/agregarCiudad-text.png"),
+        require("../../../assets/howToUseImages/delete-city-text.png")
+    
+    ]
     return (
-        <View style={Styles.main_view}>
+        <LinearGradient colors={['#87CEEB', '#b5d1ff']} style={Styles.main_view}>
             <Text>
                 ubicacion actual
             </Text>
             <NearWeather/>
-            <Text>Explicacion de como operar la app</Text>
-        </View>
+            <View style={Styles.slider_box}>
+            <SliderBox  
+            resizeMode={'contain'}
+            sliderBoxHeight={350}
+             images={images} />
+            </View>
+        </LinearGradient>
     )
 }
 const Styles = StyleSheet.create({
     main_view:{
         flex:1,
-        backgroundColor: 'lightblue',
+        backgroundColor: '#87CEEB',
         alignItems:'center'
+    },
+    slider_box:{
+        flex:1,
     }
 })
